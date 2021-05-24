d49d87baa6479d3d3be02a9785eb2bc24b439c00
Tom <fincht96@gmail.com>
2021-05-24 23:33:46 +0100

    created user overview landing page

--- src/views/auth/Home.vue
+++ src/views/auth/Home.vue
@@ -42,7 +42,77 @@
           </div>
         </div>
 
-        <div class="content"></div>
+        <div class="content">
+          <div class="overview-section">
+            <h1 class="heading">Recent folders</h1>
+            <div class="cards-group">
+              <div class="folder-card">
+                <img
+                  class="icon"
+                  src="../../assets/folder_icon.png"
+                  alt="folder icon"
+                />
+                <div class="text-group">
+                  <h3 class="title">Biology Chapter 2</h3>
+                  <div class="details">10 sets | Updated 30 mins ago</div>
+                </div>
+              </div>
+
+              <div class="folder-card">
+                <img
+                  class="icon"
+                  src="../../assets/folder_icon.png"
+                  alt="folder icon"
+                />
+                <div class="text-group">
+                  <h3 class="title">Physics Quantum Mechanics</h3>
+                  <div class="details">10 sets | Updated 2 hours ago</div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="overview-section">
+            <h1 class="heading">Recent sets</h1>
+            <div class="cards-group">
+              <div class="set-card">
+                <img
+                  class="icon"
+                  src="../../assets/set_icon.png"
+                  alt="set icon"
+                />
+                <div class="text-group">
+                  <h3 class="title">Immune system</h3>
+                  <div class="details">10 sets | Updated 45 mins ago</div>
+                </div>
+              </div>
+
+              <div class="set-card">
+                <img
+                  class="icon"
+                  src="../../assets/set_icon.png"
+                  alt="set icon"
+                />
+                <div class="text-group">
+                  <h3 class="title">Sub-atomic particles</h3>
+                  <div class="details">67 sets | Updated 6 hours ago</div>
+                </div>
+              </div>
+
+              <div class="set-card">
+                <img
+                  class="icon"
+                  src="../../assets/set_icon.png"
+                  alt="set icon"
+                />
+                <div class="text-group">
+                  <h3 class="title">Cell biology</h3>
+                  <div class="details">56 sets | Updated 18th Jan 2020</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -204,8 +274,118 @@ export default {
   }
 
   .content {
-    border: solid orange 1px;
     height: 500px;
+
+    .overview-section {
+      display: flex;
+      flex-direction: column;
+      margin-bottom: 50px;
+
+      .heading {
+        color: #666666;
+        font-size: 18px;
+        font-weight: 300;
+        margin-bottom: 30px;
+        text-align: left;
+      }
+
+      .cards-group {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        row-gap: 30px;
+        column-gap: 20px;
+
+        .folder-card {
+          border: solid 1px #cccccc;
+          padding: 20px 30px;
+          border-radius: 5px;
+          display: flex;
+
+          .icon {
+            height: 26px;
+            height: 21px;
+            margin: auto 0;
+          }
+
+          .text-group {
+            display: flex;
+            flex-direction: column;
+            text-align: left;
+
+            margin: auto 0;
+            margin-left: 30px;
+
+            .title {
+              color: #666666;
+              font-size: 16px;
+              font-weight: 500;
+
+              padding: 0;
+              margin: 0;
+              margin-bottom: 15px;
+              white-space: nowrap;
+              overflow: hidden;
+              text-overflow: ellipsis;
+            }
+
+            .details {
+              color: #808080;
+              font-size: 14px;
+              font-weight: 400;
+
+              white-space: nowrap;
+              overflow: hidden;
+              text-overflow: ellipsis;
+            }
+          }
+        }
+
+        .set-card {
+          border: solid 1px #cccccc;
+          padding: 20px 30px;
+          border-radius: 5px;
+          display: flex;
+
+          .icon {
+            height: 22px;
+            height: 22px;
+            margin: auto 0;
+          }
+
+          .text-group {
+            display: flex;
+            flex-direction: column;
+            text-align: left;
+
+            margin: auto 0;
+            margin-left: 30px;
+
+            .title {
+              color: #666666;
+              font-size: 16px;
+              font-weight: 500;
+
+              padding: 0;
+              margin: 0;
+              margin-bottom: 15px;
+              white-space: nowrap;
+              overflow: hidden;
+              text-overflow: ellipsis;
+            }
+
+            .details {
+              color: #808080;
+              font-size: 14px;
+              font-weight: 400;
+
+              white-space: nowrap;
+              overflow: hidden;
+              text-overflow: ellipsis;
+            }
+          }
+        }
+      }
+    }
   }
 }
 
